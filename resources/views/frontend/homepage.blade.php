675f078cb875d8cc03259ac9576f336069c5e94d
sahrul1610 <sahrulfyou@gmail.com>
2023-06-26 12:02:50 +0700

    perbaikan tampilan home

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="mb-5">
         <div class="container">
-            <div class="hero__item set-bg" data-setbg="{{ asset('frontend/img/hero/banner.jpg') }}">
+            <div class="hero__item set-bg" data-setbg="{{ asset('frontend/img/hero/banner.png') }}">
                 <div class="hero__text">
                     <span>FRUIT FRESH</span>
                     <h2>Vegetable <br />100% Organic</h2>
